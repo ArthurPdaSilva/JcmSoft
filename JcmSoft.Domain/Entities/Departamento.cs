231f1145c480fafb78a42a3e6f18984f1ba2ae78
Arthur Pereira da Silva <arthurnpdasilva@gmail.com>
2025-08-29 09:20:51 -0300

    Adicionar arquivos de projeto.

---
+++ JcmSoft.Domain/Entities/Departamento.cs
@@ -0,0 +1,9 @@
+﻿namespace JcmSoft.Domain.Entities
+{
+    public class Departamento
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ JcmSoft.EFCore/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using JcmSoft.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JcmSoft.EFCore.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("string de conexão");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
